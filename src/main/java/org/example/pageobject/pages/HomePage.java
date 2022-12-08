c2ba3fc7d8a04123ef83fc53a2e8c0d565314378
Aleksandr Guido <iggelds@gmail,com>
2022-12-08 11:21:25 +0100

    PageObject and Cucumber added

---
+++ src/main/java/org/example/pageobject/pages/HomePage.java
@@ -0,0 +1,10 @@
+package org.example.pageobject.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+
+    protected HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
